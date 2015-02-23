c0ceebee3fd4ec131faf6248d414c37408830c1c
Tom Chinery <tomchinery@hotmail.co.uk>
2015-02-23 18:31:21 +0000

    Changed footer social links to the actual pages

--- footer.php
+++ footer.php
@@ -5,14 +5,14 @@
 
     <article>
       <h3>New Business / enquiries</h3>
-      <a href="#">07427 644417</a>
-      <a href="#">hello@tomchinery.com</a>
+      <a href="tel:07427644417">07427 644417</a>
+      <a href="mailto:hello@tomchinery.com?Subject=Hello">hello@tomchinery.com</a>
 
       <h3>Social</h3>
-      <a href="#"><span class="icon-twitter"></span></a>
-      <a href="#"><span class="icon-linkedin2"></span></a>
-      <a href="#"><span class="icon-github"></span></a>
-      <a href="#"><span class="icon-codepen"></span></a>
+      <a href="https://twitter.com/tom_chinery" target="_blank"><span class="icon-twitter"></span></a>
+      <a href="https://www.linkedin.com/profile/view?id=161809543&trk=nav_responsive_tab_profile" target="_blank"><span class="icon-linkedin2"></span></a>
+      <a href="https://github.com/TomChinery" target="_blank"><span class="icon-github"></span></a>
+      <a href="http://codepen.io/tomchinery/" target="_blank"><span class="icon-codepen"></span></a>
 
       <p>
         &copy; 2014 - <?php echo date("Y"); ?> Tom Chinery. All rights reserved.
